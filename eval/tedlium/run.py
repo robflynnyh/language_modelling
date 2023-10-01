de74f96334fc26a04fb057d61ffda054e5f01f47
Rob Flynn <rjflynn2@shef.ac.uk>
2023-10-01 12:35:54 +0000

    idk

--- eval/tedlium/run.py
+++ eval/tedlium/run.py
@@ -8,15 +8,16 @@ from lming.models.transformer import transformer_lm
 from lming.utils.general import load_model, save_model, load_checkpoint
 
 from lming.loading.tokenizer import load_tokenizer
-
+import pandas as pd
 from einops import rearrange
 import os
 import wandb
 import re
 from tqdm import tqdm
 
-TEST_PATH = '/store/store4/data/TEDLIUM_release1/legacy/test/'
 DEV_PATH = '/store/store4/data/TEDLIUM_release1/legacy/dev/'
+TEST_PATH = '/store/store4/data/TEDLIUM_release1/legacy/test/'
+TRAIN_EXAMPLE = '/store/store4/data/TEDLIUM_release1/legacy/train/stm/EllenGustafson_2010X.stm'
 
 
 def open_stm(path:str) -> List[str]:
@@ -42,6 +43,11 @@ def proc_stm_and_timings(stm_path:str):
     # regex to do all of the above
     # i.e replace space followed by a apostrophe followed by a letter with just the apostrophe and letter
     all_text = re.sub(r" '([a-z])", r"'\1", all_text)
+    # remomve anything inside of any brackets
+    all_text = re.sub(r"\(.*?\)", "", all_text)
+    all_text = re.sub(r"\[.*?\]", "", all_text)
+    all_text = re.sub(r"\{.*?\}", "", all_text)
+    all_text = re.sub(r"\<.*?\>", "", all_text)
     # remove multiple spaces
     all_text = re.sub(r" +", r" ", all_text)
     return all_text, timings
@@ -66,8 +72,51 @@ def loss_ce(logits, labels, ignore_index=-100, label_smoothing=0.0, reduction='s
         )
 
 @torch.no_grad()
-def get_perplexity(args:argparse.Namespace, model:transformer_lm, text:str, tokenizer:spm.SentencePieceProcessor):
+def get_perplexity(args:argparse.Namespace, model:transformer_lm, text:str, tokenizer:spm.SentencePieceProcessor, prev_cache:Dict[str, torch.Tensor] = None):
     tokenized_text = tokenizer.encode(text)
+    bos = tokenizer.bos_id()
+    tokenized_text = [bos] + tokenized_text
+    seq_len = args.seq_len if args.seq_len != -1 else args.config['text_chunking']['size']
+    cache_len = args.cache_len if args.cache_len != -1 else args.config['training']['max_seq_len']
+
+    print(f'Processing with seq_len: {seq_len} and cache_len: {cache_len}')
+
+    loss_fn = lambda logits, targets: loss_ce(
+        logits=logits, 
+        labels=targets, 
+        ignore_index=-100,
+    )
+
+    all_logits = []
+    # process text in chunks of seq_len
+    prev_cache = prev_cache
+    pbar = tqdm(range(0, len(tokenized_text), seq_len), total=len(tokenized_text)//seq_len)
+    for i in pbar:
+        cur_chunk = tokenized_text[i:i+seq_len]
+        cur_chunk = torch.LongTensor(cur_chunk).unsqueeze(0).to(model.device)
+   
+        logits, _, cached_kvs = model(x = cur_chunk, cache = prev_cache)
+        all_logits.append(logits)
+        if cache_len != 0:
+            prev_cache = cached_kvs
+            prev_cache['cache'] = prev_cache['cache'][:, :, :, :, -cache_len:]
+            prev_cache['cache_lengths'] = prev_cache['cache_lengths'] * 0 + prev_cache['cache'].shape[-2]
+
+    pbar.close()
+    all_logits = torch.cat(all_logits, dim=1)
+    target = torch.LongTensor(tokenized_text)[None, 1:].to(model.device)    
+    all_logits = all_logits[:, :-1]
+    
+    loss = loss_fn(all_logits, target) # reduyction is sum
+    total_words = len(text.split(' ')) - 1 # -1 bcos last word is not predicted
+    perplexity = torch.exp(loss / total_words)
+    print(f'Perplexity: {perplexity.item()}, Total words: {total_words}, Total Tokens: {len(tokenized_text)}')
+    return loss, total_words, len(tokenized_text)
+
+@torch.no_grad()
+def get_init_seq(args:argparse.Namespace, model:transformer_lm, text:str, tokenizer:spm.SentencePieceProcessor):
+    text = text + '. ' if not text.endswith('.') else text + ' '
+    tokenized_text = tokenizer.encode(text + '. ') # add a period to the end
     bos = tokenizer.bos_id()
     tokenized_text = [bos] + tokenized_text
     seq_len = args.seq_len if args.seq_len != -1 else args.config['text_chunking']['size']
@@ -97,16 +146,7 @@ def get_perplexity(args:argparse.Namespace, model:transformer_lm, text:str, toke
             prev_cache['cache_lengths'] = prev_cache['cache_lengths'] * 0 + prev_cache['cache'].shape[-2]
 
     pbar.close()
-    all_logits = torch.cat(all_logits, dim=1)
-    target = torch.LongTensor(tokenized_text)[None, 1:].to(model.device)    
-    all_logits = all_logits[:, :-1]
-    
-    loss = loss_fn(all_logits, target) # reduyction is sum
-    total_words = len(text.split(' ')) - 1 # -1 bcos last word is not predicted
-    perplexity = torch.exp(loss / total_words)
-    print(f'Perplexity: {perplexity.item()}')
-    return loss, total_words
-
+    return prev_cache
 
 def convert_from_ddp(model_state_dict):
     '''
@@ -126,6 +166,7 @@ def main(args):
         #torch.save(checkpoint, args.checkpoint)
 
     model_config = checkpoint['config']
+    print(model_config)
     args.config = model_config
 
     tokenizer = load_tokenizer()
@@ -144,15 +185,30 @@ def main(args):
     print(f'Evaluating {args.split} data')
     _, text_files = fetch_data(path=data_path)
 
-    loss_sum, total_words_sum = 0, 0
+    init_text, _ = proc_stm_and_timings(TRAIN_EXAMPLE)
+    prev_cache = get_init_seq(args = args, model = model, text = init_text, tokenizer = tokenizer) if args.buffer else None
+
+    loss_sum, total_words_sum, tokens_sum = 0, 0, 0
     for text_file in text_files:
         print(f'Processing {text_file}')
         text, _ = proc_stm_and_timings(text_file)
-        loss, total_words = get_perplexity(args = args, model = model, text = text, tokenizer = tokenizer)
+
+        loss, total_words, total_tokens = get_perplexity(
+            args = args, 
+            model = model, 
+            text = text, 
+            tokenizer = tokenizer, 
+            prev_cache = {
+                'cache': prev_cache['cache'].clone(),
+                'cache_lengths': prev_cache['cache_lengths'].clone(),
+            } if prev_cache is not None else None
+        )
+        tokens_sum += total_tokens
         loss_sum += loss
         total_words_sum += total_words
 
     perplexity = torch.exp(loss_sum / total_words_sum)
+    print(f'Total Words: {total_words_sum}, Total Tokens: {tokens_sum}')
     print(f'\n\n -----------------\nOverall Perplexity: {perplexity.item()}')
   
 
@@ -164,7 +220,7 @@ if __name__ == '__main__':
     parser.add_argument('-fddp', '--from_ddp', action='store_true', help='convert model from DDP to single GPU')
     parser.add_argument('-seq', '--seq_len', type=int, default=-1, help='-1 to use setting from config in checkpoint file')
     parser.add_argument('-cache', '--cache_len', type=int, default=-1, help='-1 to use setting from config in checkpoint file')
-
+    parser.add_argument('-buffer','--buffer', action='store_true', help='use buffer') 
     args = parser.parse_args()
     main(args)
     
